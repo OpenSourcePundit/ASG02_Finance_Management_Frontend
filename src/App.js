c6c5d4e59389d4de8fcb0797584bc2a9c586735a
Shashank <112277097+OpenSourcePundit@users.noreply.github.com>
2024-01-12 00:18:14 +0000

    Update 33 files. App completed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import "./styles.css";
+import Navbar from "./components/navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import IncomeExpense from "./pages/incomeExpenseForm/IncomeExpense";
+import Income from "./pages/income/Income.jsx";
+import Expense from "./pages/expense/Expense.jsx";
+import Saving from "./pages/saving/Saving.jsx";
+import FinanceReports from "./pages/report/FinanceReports.jsx";
 
-export default function App() {
+import "./App.css";
+
+function App() {
   return (
     <div className="App">
-      <h1>Hello Shashank</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<IncomeExpense />} />
+        <Route path="/income" element={<Income />} />
+        <Route path="/expense" element={<Expense />} />
+        <Route path="/saving" element={<Saving />} />
+        <Route path="/finance" element={<FinanceReports />} />
+      </Routes>
     </div>
   );
 }
+
+export default App;
---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div>
+      <h2>Loading....</h2>
+      <h4>First Load might take 1-2 mins. Please have patience.</h4>
+    </div>
+  );
+};
+
+export default Loader;
